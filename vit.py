14f7072c27d8ecd2895be1fd2038bf90075b469b
hudaili <hudaili@MacBook-Pro-4.local>
2025-04-03 12:14:03 +0800

    创建VisionTransformer类

---
+++ vit.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+class VisionTransformer(nn.Module):
+    def __init__(self, dim):
+        super(VisionTransformer, self).__init__()
+        pass
+
+    def forward(self, images):
+        pass
